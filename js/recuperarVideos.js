44bdebf580bb5eee1a48fe32bfcd27f95c06d7f7
Gabriel Segado Moreno <gabriel.segado@edu.upct.es>
2022-03-01 17:32:47 +0100

    he vuelto a formar la carpeta grupo 3

---
+++ js/recuperarVideos.js
@@ -0,0 +1,13 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MjAxNjdmODY1ZDJmNGI0NjUxOWMxZDgiLCJpYXQiOjE2NDYxNDkwOTksImV4cCI6MTY0NjE2MzQ5OX0.12NXr4AM15EpLTGG9fcIzVERKsGaOVdMydtwdEXyxcc");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://labinfsoft.herokuapp.com/api/videos?limite=10&desde=0", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
