86761a15cdb784ab8aed89df8a0ba32c7bd9199f
Gabriel Segado Moreno <gabriel.segado@edu.upct.es>
2022-03-04 11:37:04 +0100

    Entrar

--- js/recuperarVideos.js
+++ js/recuperarVideos.js
@@ -12,3 +12,23 @@ fetch("https://labinfsoft.herokuapp.com/api/videos?limite=10&desde=0", requestOp
   .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+  //result.productos[0].categoria.id;
+
+  //2.- Acceder a los valores de un objeto JSON si NO conoces su estructura"
+// display(obj,"");
+
+//Necesitamos encontrar una manera de contar el número de categorías que hay en la petición
+//Para eso no sé si usar un bucle while o un bucle for para sacar los datos.
+
+// function display(obj,sp) {
+//  for (n in obj) { //n debería recorrer toda la petición JSON
+//   if (typeof obj[n] == 'object') {
+//    display(obj[n],n+".");
+//   }else{
+//    html.innerhtml+="<p>"+sp+n+"="+obj[n];
+//   }
+//  }
+// }
+
+// html.innerhtml+="\
