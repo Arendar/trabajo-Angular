b2b15f1de12eb2f7d85dd67b784d96bf50854f10
Gabriel Segado Moreno <gabriel.segado@edu.upct.es>
2022-03-04 20:15:18 +0100

    Codigo añadido en recuperarVideos

--- js/recuperarVideos.js
+++ js/recuperarVideos.js
@@ -1,6 +1,9 @@
+var idGenero = "";
+var nGeneros = 0;
+var primero = true;
 var myHeaders = new Headers();
 var data = sessionStorage.getItem('token');
-myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MjAxNjdmODY1ZDJmNGI0NjUxOWMxZDgiLCJpYXQiOjE2NDYxNDkwOTksImV4cCI6MTY0NjE2MzQ5OX0.12NXr4AM15EpLTGG9fcIzVERKsGaOVdMydtwdEXyxcc");
+myHeaders.append("Authorization", "Bearer "+data);
 
 var requestOptions = {
   method: 'GET',
@@ -16,19 +19,52 @@ fetch("https://labinfsoft.herokuapp.com/api/videos?limite=10&desde=0", requestOp
   //result.productos[0].categoria.id;
 
   //2.- Acceder a los valores de un objeto JSON si NO conoces su estructura"
-// display(obj,"");
 
 //Necesitamos encontrar una manera de contar el número de categorías que hay en la petición
 //Para eso no sé si usar un bucle while o un bucle for para sacar los datos.
 
-// function display(obj,sp) {
-//  for (n in obj) { //n debería recorrer toda la petición JSON
-//   if (typeof obj[n] == 'object') {
-//    display(obj[n],n+".");
-//   }else{
-//    html.innerhtml+="<p>"+sp+n+"="+obj[n];
-//   }
-//  }
-// }
+var videos = result.total;
 
-// html.innerhtml+="\
+document.body.onload = muestravideos
+
+function muestravideos() {
+ for (let i = 0; i < videos; i++) { //n debería recorrer toda la petición json
+   if (result.productos[i].categoria.id != idGenero) {
+     idGenero = result.productos[i].categoria.id;
+       //en lugar de crear nuevos divs ponemos en el doc html
+       //ya uno de base con una id definida.
+       //a partir de ahí vamos creando el resto de divs asignandoles la clase e id correspondiente
+     
+     var div = document.getelementbyid("cat");    
+     var clon = div.clonenode("cat");
+     clon.setattribute("id", idGenero);
+     clon.setattribute("class", "categoria");
+
+     var h3 = document.createelement("h3");
+     var text = document.createtextnode(result.productos[i].categoria.nombre);
+     h3.appendchild(text);
+     clon.appendchild(h3);
+     document.body.appendchild(clon);
+   } 
+ }
+
+ var categorias = document.getelementsbyclassname("categoria");
+ for (var j=0; j < videos; j++){
+   for (var k=0; k < categorias.length; k++){
+     if(result.productos[j].categoria.id == categorias[k].id){
+       
+       var tag = document.createelement("p");
+       var text = document.createtextnode(result.productos[j].nombre);
+       tag.appendchild(text);
+       categorias[k].appendchild(tag);
+
+       //<iframe width="420" height="315" src="">
+       var iframe = document.createelement("iframe");
+       iframe.setattribute("width", "420");
+       iframe.setattribute("height", "315");
+       iframe.setattribute("src", result.productos[j].url);
+       categorias[k].appendchild(iframe);
+     }
+   }
+ }
+}
