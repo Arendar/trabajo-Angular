76aa3f8921231cff0cca980b6c0eb145762025a1
Gabriel Segado Moreno <gabriel.segado@edu.upct.es>
2022-03-15 17:01:29 +0100

    Cambios en Token

--- js/Token.js
+++ js/Token.js
@@ -1,12 +1,10 @@
-  function getToken(){
-      console.log("entro");
+  function getToken(){ 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    var login=[];
-
+    var login=[];    
     var raw = JSON.stringify({
-      "correo": "fminima@gmail.com",
-      "password": "pHLpLOevSv9mPygdNBPV"
+      "correo": document.getElementById("email").value,
+      "password": document.getElementById("btn").value
     });
   login.push(raw);
     
