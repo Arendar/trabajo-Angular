5af2a05f900624d6a952e4f7dd5ee3c9126e7cd6
Arendar <gsegadomoreno@gmail.com>
2022-06-14 20:36:28 +0200

    He modificado signup y index para meterlo en localhost y para añadir comentarios

--- Sprint2/index.js
+++ Sprint2/index.js
@@ -15,9 +15,25 @@ app.use(express.urlencoded({extendes:true}));
 app.post("/formulario", (req, res) => {
   console.log(req.body);
   const{username,password} =req.body;
-  if(!username || !password) return res.send('no existen los datos');
+  if(!username || !password){ 
+    return res.send('no existen los datos');
+  }else{
+    //Realizar petición comprobación existencia usuario
 
+    //Si existe devuelve el id del usuario para crear un token
+  }
 })
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const{username,password} =req.body;
+  if(!username || !password){ 
+    return res.send('no existen los datos');
+  }else{
+    //Realizar petición para crear usuario a
+  }
+})
+
 app.get("/", (req,res) =>{
   res.send("visitaste la pagina de inicio");
 });
